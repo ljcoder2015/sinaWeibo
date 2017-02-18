99406ba5739cad80e3c8a7989bb44ca2b35e62a4
ljcoder <ljcoder@163.com>
2017-02-18 13:46:21 +0800

    访客视图

---
+++ SinaWeibo/SinaWeibo/MVVM/View/Main/WBVisitorView.swift
@@ -0,0 +1,32 @@
+//
+//  WBVisitorView.swift
+//  SinaWeibo
+//
+//  Created by ljcoder on 17/2/18.
+//  Copyright © 2017年 ljcoder. All rights reserved.
+//
+
+import UIKit
+
+class WBVisitorView: UIView {
+
+    override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+        
+        setupUI()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
+extension WBVisitorView {
+    
+    fileprivate func setupUI() {
+        
+    }
+    
+}
